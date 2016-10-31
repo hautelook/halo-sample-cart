66aa80d570684af9b34ed5eee6145af13d6fed57
faran-rashid <faran.rashid@hautelook.com>
2016-10-31 13:18:12 -0700

    Candidate T assignment scoring

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,13 +1,16 @@
 <?php
 
 use Behat\Behat\Context\ClosuredContextInterface,
-    Behat\Behat\Context\TranslatedContextInterface,
-    Behat\Behat\Context\BehatContext,
-    Behat\Behat\Exception\PendingException;
+Behat\Behat\Context\TranslatedContextInterface,
+Behat\Behat\Context\BehatContext,
+Behat\Behat\Exception\PendingException;
 use Behat\Gherkin\Node\PyStringNode,
-    Behat\Gherkin\Node\TableNode;
+Behat\Gherkin\Node\TableNode;
 use PHPUnit_Framework_Assert as Assert;
-use Hautelook\Cart;
+use Hautelook\Cart,
+Hautelook\Item,
+Hautelook\Items,
+Hautelook\NumberUtils;
 
 /**
  * Features context.
@@ -15,88 +18,180 @@ use Hautelook\Cart;
 class FeatureContext extends BehatContext
 {
 
-    private $cart;
+	private $cart;
+	private $shipping=0;
+	private $must_apply_flat=false;
+	private $flat_applied=false;
 
-    /**
-     * Initializes context.
-     * Every scenario gets it's own context object.
-     *
-     * @param array $parameters context parameters (set them up through behat.yml)
-     */
-    public function __construct(array $parameters)
-    {
-    }
+	/**
+	 * Initializes context.
+	 * Every scenario gets it's own context object.
+	 *
+	 * @param array $parameters context parameters (set them up through behat.yml)
+	 */
+	public function __construct(array $parameters)
+	{
+		$this->cart = new Cart();
+	}
 
-    /**
-     * @Given /^I have an empty cart$/
-     */
-    public function iHaveAnEmptyCart()
-    {
-        $this->cart = new Cart();
-    }
+	/**
+	 * @Given /^I have an empty cart$/
+	 */
+	public function iHaveAnEmptyCart()
+	{
+		$this->cart = new Cart();
+	}
 
-    /**
-     * @Then /^My subtotal should be "([^"]*)" dollars$/
-     */
-    public function mySubtotalShouldBeDollars($subtotal)
-    {
-        Assert::assertEquals($subtotal, $this->cart->subtotal());
-    }
+	/**
+	 * @Then /^My subtotal should be "([^"]*)" dollars$/
+	 */
+	public function mySubtotalShouldBeDollars($subtotal)
+	{
+		$this->cart->setShipping();
+		Assert::assertEquals($subtotal, $this->cart->subtotal());
+	}
 
-    /**
-     * @When /^I add a "([^"]*)" dollar item named "([^"]*)"$/
-     */
-    public function iAddADollarItemNamed($dollars, $product_name)
-    {
-        throw new PendingException();
-    }
-    
-    /**
-     * @When /^I add a "([^"]*)" dollar "([^"]*)" lb item named "([^"]*)"$/
-     */
-    public function iAddADollarItemWithWeight($dollars, $lb, $product_name)
-    {
-        throw new PendingException();
-    }
-    
-    /**
-     * @Then /^My total should be "([^"]*)" dollars$/
-     */
-    public function myTotalShouldBeDollars($total)
-    {
-        throw new PendingException();
-    }
+	/**
+	 * @When /^I add a "([^"]*)" dollar item named "([^"]*)"$/
+	 */
+	public function iAddADollarItemNamed($dollars, $product_name)
+	{
+		$subtotal_orignal=$this->cart->subtotal();
+		$this-> cart-> add($dollars, $product_name);
+		$scaleDiscountRate = NumberUtils::getNumberOfFractionalDigits((double)100 - $this->cart-> getDiscountRate()) + 2;
+		$scaleItemsSubtotal = NumberUtils::getNumberOfFractionalDigits((double)$this->cart->getItems()->getSubtotal()) + $scaleDiscountRate;
+		$precision=2;
+		$expected=(double)round(bcmul($this->cart->getItems()->getSubtotal(),bcdiv((double)100- $this->cart-> getDiscountRate(), 100,$scaleDiscountRate),$scaleItemsSubtotal),$precision);
+		Assert::assertEquals($expected, $this->cart->subtotal());
+	}
 
-    /**
-     * @Then /^My quantity of products named "([^"]*)" should be "([^"]*)"$/
-     */
-    public function myQuantityOfProductsShouldBe($product_name, $quantity)
-    {
-        throw new PendingException();
-    }
-    
+	/**
+	 * @When /^I add a "([^"]*)" dollar "([^"]*)" lb item named "([^"]*)"$/
+	 */
+	public function iAddADollarItemWithWeight($dollars, $lb, $product_name)
+	{
+		$subtotal_orignal=$this->cart->subtotal();
+		$this-> cart -> add($dollars, $product_name, $lb);
+		Assert::assertEquals(round($subtotal_orignal+$dollars*(100-$this->cart-> getDiscountRate())/100,2), $this->cart->subtotal());
+	}
 
-    /**
-     * @Given /^I have a cart with a "([^"]*)" dollar item named "([^"]*)"$/
-     */
-    public function iHaveACartWithADollarItem($item_cost, $product_name)
-    {
-        throw new PendingException();
-    }
+	/**
+	 * @Then /^My total should be "([^"]*)" dollars$/
+	 */
+	public function myTotalShouldBeDollars($total)
+	{
+		$this-> cart-> setShipping();
+		Assert::assertEquals($total,$this->cart-> getTotal());
+	}
 
-    /**
-     * @When /^I apply a "([^"]*)" percent coupon code$/
-     */
-    public function iApplyAPercentCouponCode($discount)
-    {
-        throw new PendingException();
-    }
+	/**
+	 * @Then /^My quantity of products named "([^"]*)" should be "([^"]*)"$/
+	 */
+	public function myQuantityOfProductsShouldBe($product_name, $quantity)
+	{
+		Assert::assertEquals($quantity, $this->cart->getQuantityOfProducts($product_name));
+	}
 
-    /**
-     * @Then /^My cart should have "([^"]*)" item\(s\)$/
-     */
-    public function myCartShouldHaveItems($item_count)
-    {
-        throw new PendingException();
-    }
+	/**
+	 * @Given /^I have a cart with a "([^"]*)" dollar item named "([^"]*)"$/
+	 */
+	public function iHaveACartWithADollarItem($item_cost, $product_name)
+	{
+		$this->cart-> add($item_cost, $product_name);
+		Assert::assertEquals($item_cost, $this->cart->subtotal());
+	}
+
+	/**
+	 * @When /^I apply a "([^"]*)" percent coupon code$/
+	 */
+	public function iApplyAPercentCouponCode($discount)
+	{
+		$subtotal_before_discount=$this-> cart -> subtotal();
+		$this-> cart -> applyAPercentCouponCode($discount);
+		$scaleDiscountRate = NumberUtils::getNumberOfFractionalDigits((double)100 - $this->cart-> getDiscountRate()) + 2;
+		$scaleItemsSubtotal = NumberUtils::getNumberOfFractionalDigits((double)$this->cart->getItems()->getSubtotal()) + $scaleDiscountRate;
+		$precision=2;
+		Assert::assertEquals((double)round(bcmul($subtotal_before_discount,bcdiv(100-(double)$discount, 100,$scaleDiscountRate),$scaleItemsSubtotal),$precision),$this->cart->subtotal());
+	}
+
+	/**
+	 * @Then /^My cart should have "([^"]*)" item\(s\)$/
+	 */
+	public function myCartShouldHaveItems($item_count)
+	{
+		throw new PendingException();
+	}
+
+	//Added variable(s) and functions
+	
+	private $invalidArgumentExceptionOccured=false;
+
+	/**
+	 * @When /^I add a "([^"]*)" dollar "([^"]*)" lb item named "([^"]*)", throws InvalidArgumentException$/
+	 */
+	public function iAddADollarItemWithWeightInvalidArgumentException($dollars, $lb, $product_name)
+	{
+		$subtotal_orignal=$this->cart->subtotal();
+		try{
+			$this-> cart -> add($dollars, $product_name, $lb);
+			Assert::assertTrue(false);
+			$this-> invalidArgumentExceptionOccured=false;
+		}catch(InvalidArgumentException $invalidArgumentException){
+			Assert::assertTrue(true);
+			$this-> invalidArgumentExceptionOccured=true;
+		}
+	}
+
+	/**
+	 * @When /^I add a "([^"]*)" dollar item named "([^"]*)", throws InvalidArgumentException$/
+	 */
+	public function iAddADollarItemNamedInvalidArgumentException($dollars, $product_name)
+	{
+		$subtotal_orignal=$this->cart->subtotal();
+		try{
+			$this-> cart -> add($dollars, $product_name);
+			Assert::assertTrue(false);
+			$this-> invalidArgumentExceptionOccured=false;
+		}catch(InvalidArgumentException $invalidArgumentException){
+			Assert::assertTrue(true);
+			$this-> invalidArgumentExceptionOccured=true;
+		}
+	}
+	
+	/**
+	 * @When /^Numberic value is "([^"]*)", number of fractional digits is "([^"]*)"$/
+	 */
+	public function numberOfDigitCounterGetNumberOfFractionalDigits($doubleValue, $returnValue)
+	{
+		Assert::assertEquals($returnValue, NumberUtils::getNumberOfFractionalDigits($doubleValue));
+	}
+
+	/**
+	 * @When /^Numberic value is "([^"]*)", throws InvalidArgumentException$/
+	 */
+	public function numberOfDigitCounterGetNumberOfFractionalDigitsInvalidArgumentException($doubleValue)
+	{
+		try{
+			NumberUtils::getNumberOfFractionalDigits($doubleValue);
+			Assert::assertTrue(false);
+			$this-> invalidArgumentExceptionOccured=false;
+		}catch(InvalidArgumentException $invalidArgumentException){
+			Assert::assertTrue(true);
+			$this-> invalidArgumentExceptionOccured=true;
+		}
+	}
+	
+	/**
+	 * @Then /^InvalidArgumentException Occured: "([^"]*)"$/
+	 */
+	public function InvalidArgumentExceptionOccured($yes_no)
+	{
+		if($this-> invalidArgumentExceptionOccured)
+		{
+			Assert::assertEquals($yes_no,"yes");
+		}else{
+			Assert::assertEquals($yes_no,"no");
+		}
+		$this-> invalidArgumentExceptionOccured = false;
+	}
 }
--- src/Hautelook/Cart.php
+++ src/Hautelook/Cart.php
@@ -1,10 +1,141 @@
 <?php
 namespace Hautelook;
 
+/**
+ * Defines behavior of the shopping cart.
+ *
+ * @author
+ */
 class Cart
 {
-    public function subtotal()
-    {
-        return 0;
-    }
-} 
+	private $discountRate=0;
+	private $flat=0;
+	private $items;
+	private $shipping=0;
+
+	/**
+	 * Constructor
+	 */
+	public function __construct()
+	{
+		$this->items=new Items();
+	}
+
+	/**
+	 * Add a product
+	 * @param double $dollars Price of a product to add.
+	 * @param string $product_name Name of a product to add.
+	 * @param double $lb Weight of a product to add.
+	 */
+	public function add($cost, $product_name, $lb=null)
+	{
+		//Check if the given numeric can be handled.
+		$itemsSubtotal = $this->items->getSubtotal() + $cost;
+		$subtotal = $this->getSubtotal($itemsSubtotal);
+		$total = (double)round($subtotal + $this->shipping + $this->flat,2);
+		try{
+			NumberUtils::checkNumericValue($itemsSubtotal);	//check items subtotal
+			NumberUtils::checkNumericValue($subtotal);//check subtotal
+			NumberUtils::checkNumericValue($total); //check total
+		}catch(InvalidArgumentException $invalidArgumentException){
+			throw InvalidArgumentException("Cart::add(".$cost.", ".$product_name.", ".$lb."): ".$invalidArgumentException);
+		}
+		//Add
+		$this->items->add($cost, $product_name, $lb);
+	}
+
+	/**
+	 * Set discount rate specified.
+	 * @param doulbe $discount Discount rate in percentage.
+	 */
+	public function applyAPercentCouponCode($discountRate){
+		$this->discountRate = (double)$discountRate;
+	}
+
+	/**
+	 * Obtain the discount rate.
+	 * @retun double discount rate.
+	 */
+	public function getDiscountRate(){
+		return (double)$this->discountRate;
+	}
+
+	/**
+	 *
+	 * Obtain collection of Item class instances which is added by add function.
+	 * @return Items collection of Item class instances which is added by add function.
+	 */
+	public function getItems(){
+		return $this->items;
+	}
+
+	/**
+	 * Obtain quantity of a specified product.
+	 * @param string $product_name Name of a product to obtain quantity.
+	 * @return int Quantity of the specified products.
+	 */
+	public function getQuantityOfProducts($product_name)
+	{
+		return (int)$this->items->getQuantityOfProducts($product_name);
+	}
+
+	/**
+	 * Obtain total payment amount which is sum of subtotal, shipping fee and flat rate.
+	 * @return double Total payment amount.(Rounded to two decimal places.)
+	 */
+	public function getTotal()
+	{
+		return (double)round($this->subtotal() + $this->shipping+$this->flat,2);
+	}
+
+	/**
+	 * Set shipping fee and flat rate.
+	 */
+	public function setShipping(){
+		if($this->items->getSubtotal() >= 100 && $this->items ->getLbMax()<10)
+		{//Scenario: When order is $100 or more, and each individual item is under 10 lb, then shipping is free
+			$this->shipping = (double)0;
+		}else{
+			if($this->items->getQuantitySumWhereLbOrHeavier(10) > 0)
+			{//Scenario: Items that are 10 lb or more always cost $20 each to ship
+				$this->shipping = (double)$this->items ->getQuantitySumWhereLbOrHeavier(10) * 20;
+			}
+			if($this->items->getSubtotal() < 100
+			&& $this->items->getQuantitySumWhereLbLighter(10)>0)
+			{//Scenario: When order is under $100, and all items under 10 lb, then shipping is $5 flat
+				$this->flat =5;
+			}
+			//Scenario: When order is under $100, and all items are 10 lb or more, then flat rate should not be charged > covered by previous conditions
+		}
+	}
+
+	/**
+	 * Obtain subtotal of all items in dollars.
+	 * @return double Subtotal of all items in dollars.(Rounded to two decimal places.)
+	 */
+	public function subtotal()
+	{
+		return $this->getSubtotal($this->items->getSubtotal());
+	}
+
+	/**
+	 * Obtain subtotal of all items in dollars.
+	 * @param $itemSubtotal Subtotal(sum) of items cost only
+	 * @return double Subtotal of all items in dollars.(Rounded to two decimal places.)
+	 */
+	private function getSubtotal($itemSubtotal)
+	{
+		$scaleDiscountRate = NumberUtils::getNumberOfFractionalDigits((double)100 - $this->discountRate) + 2;
+		$scaleItemsSubtotal = NumberUtils::getNumberOfFractionalDigits((double)$itemSubtotal) + $scaleDiscountRate;
+		$precision=2;
+		return (double)round(bcmul((double)$itemSubtotal,bcdiv((double)100-$this->discountRate, (double)100,$scaleDiscountRate),$scaleItemsSubtotal),$precision);
+	}
+
+	/**
+	 * Returns a string representation of this object.
+	 * @return string A string representation of this object.
+	 */
+	public function __toString(){
+		return (string)$this->items;
+	}
+}
